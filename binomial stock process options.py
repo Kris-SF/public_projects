03c323f1ddb6776f7129f09327b4a909ab4c8ad3
Kris-SF <49005601+Kris-SF@users.noreply.github.com>
2023-08-08 17:55:50 -0700

    Add files via upload

--- binomial stock process options.py
+++ binomial stock process options.py
@@ -7,6 +7,8 @@ Created on Wed Aug  2 18:11:03 2023
 
 import math
 import random
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 
 def binomial_return(qty_ups, up_return, up_probability, start_price, steps):
@@ -66,78 +68,39 @@ def randomize_stock_price_change(share_price, up_probability, up_return, down_re
 
 
 #Inputs
-num_of_simulations = 2
+num_of_simulations = 10
+sets_of_sims = 20
 
 # Initialize tables before the loop
 simulation_table = pd.DataFrame(columns = ['option_position', 'strike', 'terminal_price', 'delta_hedged_P/L'])
 path_table = pd.DataFrame(columns = ['trial', 'current_step', 'strike', 'option_position', 'callput', 'share_price', 'cumulative_portfolio_P/L'])
+sets_of_sims_table = pd.DataFrame(columns= ['sim', 'mean_P/L'])
 
-for i in range(num_of_simulations):
-    
-    stock_price = 100
-    up_probability = .5
-    down_probability = .5
-    up_return = .1
-    down_return = .1
-    
-    current_step = 0
-    total_steps_til_expiry = 3
-    remaining_steps_til_expiry = total_steps_til_expiry - current_step
-    
-    strike = 110
-    callput = "c"
-    option_position = 1
-    tree = create_tree(stock_price, remaining_steps_til_expiry, up_probability, up_return, down_probability, down_return)
-    option_price = option_value(tree, strike, callput)
-    position_option_delta = option_delta(stock_price, up_probability, up_return, down_probability, down_return, remaining_steps_til_expiry, strike, callput, option_price)
-    
-    #Initialize portfolio
-    
+for j in range(sets_of_sims):
 
-   
-    position_data = {
-        'current_step': current_step,
-        'remaining steps_to_expiry': remaining_steps_til_expiry,
-        'strike': strike,
-        'type': callput,
-        'option_position': option_position,
-        'option_price': option_price,
-        'option_delta': position_option_delta,
-        'share_position':option_position*position_option_delta*-100,
-        'share_price': stock_price,
-     }
-    
-    path = {
-        'trial': 1+i,
-        'current_step': current_step,
-        'strike': strike,
-        'option_position': option_position,
-        'callput': callput,
-        'share_price': stock_price,
-        'cumulative_portfolio_P/L': 0
-    }
-    
-    portfolio = pd.DataFrame(position_data,index=[0])
-    
-    df_path = pd.DataFrame([path])
-    path_table = pd.concat([path_table, df_path], ignore_index=True)
-   
+    for i in range(num_of_simulations):
         
-    while remaining_steps_til_expiry> 0:
-    
-        '''---------Simulation----------'''
-            
-        #Increment time and position data
-      
+        stock_price = 100
+        up_probability = .5
+        down_probability = .5
+        up_return = .1
+        down_return = .1
         
-    
-        stock_price = randomize_stock_price_change(stock_price, up_probability, up_return, down_return)
-        current_step = current_step + 1        
-        remaining_steps_til_expiry = remaining_steps_til_expiry - 1
+        current_step = 0
+        total_steps_til_expiry = 3
+        remaining_steps_til_expiry = total_steps_til_expiry - current_step
+        
+        strike = 110
+        callput = "c"
+        option_position = 1
         tree = create_tree(stock_price, remaining_steps_til_expiry, up_probability, up_return, down_probability, down_return)
         option_price = option_value(tree, strike, callput)
         position_option_delta = option_delta(stock_price, up_probability, up_return, down_probability, down_return, remaining_steps_til_expiry, strike, callput, option_price)
         
+        #Initialize portfolio
+        
+    
+       
         position_data = {
             'current_step': current_step,
             'remaining steps_to_expiry': remaining_steps_til_expiry,
@@ -150,61 +113,144 @@ for i in range(num_of_simulations):
             'share_price': stock_price,
          }
         
-        df_position = pd.DataFrame([position_data])
-        portfolio = pd.concat([portfolio, df_position], ignore_index=True)
+        path = {
+            'trial': 1+i,
+            'current_step': current_step,
+            'strike': strike,
+            'option_position': option_position,
+            'callput': callput,
+            'share_price': stock_price,
+            'cumulative_portfolio_P/L': 0
+        }
         
-                
-        # Compute the change in share price from one step to the next
-        portfolio['share_price_change'] = portfolio['share_price'].diff()
-        portfolio['option_price_change'] = portfolio['option_price'].diff()
-    
-        # Compute the P/L for each step
-        portfolio['share_P/L'] = portfolio['share_price_change'] * portfolio['share_position'].shift(1)
-        portfolio['option_P/L'] = 100*portfolio['option_price_change'] * portfolio['option_position'].shift(1)
-        
-        # Compute the cumulative P/L over all steps
-        portfolio['cumulative_share_P/L'] = portfolio['share_P/L'].cumsum()
-        portfolio['cumulative_option_P/L'] = portfolio['option_P/L'].cumsum()
-        portfolio['cumulative_portfolio_P/L'] = portfolio['cumulative_share_P/L'] + portfolio['cumulative_option_P/L']
-        cumulative_portfolio_profit = portfolio['cumulative_portfolio_P/L'].iloc[current_step-1]
-        delta_hedged_profit = portfolio['cumulative_portfolio_P/L'].iloc[-1]
-        
-        #record trial path
-        if current_step <= total_steps_til_expiry:
-            path = {
-                'trial': i+1,
-                'current_step': current_step,
-                'strike': strike,
-                'option_position': option_position,
-                'callput': callput,
-                'share_price': stock_price,
-                'cumulative_portfolio_P/L': cumulative_portfolio_profit
-            }
+        portfolio = pd.DataFrame(position_data,index=[0])
         
         df_path = pd.DataFrame([path])
         path_table = pd.concat([path_table, df_path], ignore_index=True)
+       
+            
+        while remaining_steps_til_expiry> 0:
+        
+            '''---------Simulation----------'''
+                
+            #Increment time and position data
+          
+            
+        
+            stock_price = randomize_stock_price_change(stock_price, up_probability, up_return, down_return)
+            current_step = current_step + 1        
+            remaining_steps_til_expiry = remaining_steps_til_expiry - 1
+            tree = create_tree(stock_price, remaining_steps_til_expiry, up_probability, up_return, down_probability, down_return)
+            option_price = option_value(tree, strike, callput)
+            position_option_delta = option_delta(stock_price, up_probability, up_return, down_probability, down_return, remaining_steps_til_expiry, strike, callput, option_price)
+            
+            position_data = {
+                'current_step': current_step,
+                'remaining steps_to_expiry': remaining_steps_til_expiry,
+                'strike': strike,
+                'type': callput,
+                'option_position': option_position,
+                'option_price': option_price,
+                'option_delta': position_option_delta,
+                'share_position':option_position*position_option_delta*-100,
+                'share_price': stock_price,
+             }
+            
+            df_position = pd.DataFrame([position_data])
+            portfolio = pd.concat([portfolio, df_position], ignore_index=True)
+            
+                    
+            # Compute the change in share price from one step to the next
+            portfolio['share_price_change'] = portfolio['share_price'].diff()
+            portfolio['option_price_change'] = portfolio['option_price'].diff()
+        
+            # Compute the P/L for each step
+            portfolio['share_P/L'] = portfolio['share_price_change'] * portfolio['share_position'].shift(1)
+            portfolio['option_P/L'] = 100*portfolio['option_price_change'] * portfolio['option_position'].shift(1)
+            
+            # Compute the cumulative P/L over all steps
+            portfolio['cumulative_share_P/L'] = portfolio['share_P/L'].cumsum()
+            portfolio['cumulative_option_P/L'] = portfolio['option_P/L'].cumsum()
+            portfolio['cumulative_portfolio_P/L'] = portfolio['cumulative_share_P/L'] + portfolio['cumulative_option_P/L']
+            cumulative_portfolio_profit = portfolio['cumulative_portfolio_P/L'].iloc[current_step-1]
+            delta_hedged_profit = portfolio['cumulative_portfolio_P/L'].iloc[-1]
+            
+            #record trial path
+            if current_step <= total_steps_til_expiry:
+                path = {
+                    'trial': i+1,
+                    'current_step': current_step,
+                    'strike': strike,
+                    'option_position': option_position,
+                    'callput': callput,
+                    'share_price': stock_price,
+                    'cumulative_portfolio_P/L': cumulative_portfolio_profit
+                }
+            
+            df_path = pd.DataFrame([path])
+            path_table = pd.concat([path_table, df_path], ignore_index=True)
+            
+            
+            #Simulation Data Table
+            
+        simulation = {
+            'option_position': option_position,
+            'strike': strike,
+            'terminal_price': stock_price,
+            'delta_hedged_P/L': delta_hedged_profit,
+        }
         
         
-        #Simulation Data Table
+        df_simulation = pd.DataFrame([simulation])
+        simulation_table = pd.concat([simulation_table, df_simulation], ignore_index=True)
+    
+       
+       
+    
+    
+    
+    """fig, ax = plt.subplots(2, 1, figsize=(10, 15))
+    
+    # Plot 1: Histogram of Terminal Prices
+    sns.histplot(simulation_table['terminal_price'], kde=True, ax=ax[0])
+    ax[0].set_title('Distribution of Terminal Prices')
+    ax[0].set_xlabel('Terminal Price')
+    ax[0].set_ylabel('Frequency')
+    
+    
+    # Plot 2: Scatter plot between Terminal Prices and Delta Hedged P/L
+    sns.scatterplot(x='terminal_price', y='delta_hedged_P/L', data=simulation_table, ax=ax[1])
+    ax[1].set_title('Scatter plot between Terminal Prices and Delta Hedged P/L')
+    ax[1].set_xlabel('Terminal Price')
+    ax[1].set_ylabel('Delta Hedged P/L')
+    
+    
+    #plt.show()"""
+    
         
-    simulation = {
-        'option_position': option_position,
-        'strike': strike,
-        'terminal_price': stock_price,
-        'delta_hedged_P/L': delta_hedged_profit,
+    # Compute the frequency of each unique delta_hedged_P/L value
+    frequency = simulation_table['delta_hedged_P/L'].value_counts(normalize=True)
+    
+    
+    """Compute average_profit_for_all_simulations"""
+    average_profit_for_all_simulations = sum(value * frequency[value] for value in frequency.index)
+    
+    print("Mean_profit_for_all_simulations:", average_profit_for_all_simulations)
+    
+    
+     
+    """there are only steps+1 possible terminal prices, i want to see the p/l distribution for each of them"""
+    grouped = simulation_table.groupby('terminal_price')['delta_hedged_P/L']
+    
+    # Calculate mean and standard deviation for each group
+    statistics = grouped.agg(['mean', 'count']).reset_index()
+    
+    log_entry = {
+        'sim': j + 1,  # trial number
+        'mean_P/L': average_profit_for_all_simulations,
     }
-    
-    
-    df_simulation = pd.DataFrame([simulation])
-    simulation_table = pd.concat([simulation_table, df_simulation], ignore_index=True)
-
-   
-   
-        
-             
-    
-    
-    
+    df_log_entry = pd.DataFrame([log_entry])
+    sets_of_sims_table = pd.concat([sets_of_sims_table, df_log_entry], ignore_index=True)
 
 
 
